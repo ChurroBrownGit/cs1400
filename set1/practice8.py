aff760862a3e0fda4b78e2eaf278d2330b6edd8f
Joel Hadley <26hadlejoes@washk12.org>
2025-01-09 19:34:56 +0000

    commit message

--- set1/practice8.py
+++ set1/practice8.py
@@ -1,1 +1,8 @@
-print("Work on this")
+side1 = int(input("What is the 1st side length? "))
+side2 = int(input("What is the 2nd side length? "))
+side3 = int(input("What is the 3rd side length? "))
+total1 = side1 + side2
+if total1 > side3:
+    print("This is a valid triangle.")
+else:
+    print("This is not a valid trangle.")
