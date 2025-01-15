feafe1358f42c9990dd4ef4d21b4a3fff0a9ced8
ChurroBrown <26hadlejoes@washk12.org>
2025-01-15 09:01:02 -0700

    Update modulus.py

--- pythonintro/modulus.py
+++ pythonintro/modulus.py
@@ -11,3 +11,20 @@ def multiple_22():
 #Goal 2
 
 #Any number from 0-9 can be n in n % 10.
+
+#Challenge 1
+for i in range(333):
+    print(i % 3)
+
+#Challenge 2
+
+def digits(numbers):
+    ones = numbers % 10
+    tens = numbers // 10 % 10
+    hundreds = numbers // 10 // 10 % 10
+    thousands = numbers // 10 // 10 // 10 % 10
+    return [thousands, hundreds, tens, ones]
+    
+numbers = int(input("Input a number"))
+r = digits(numbers)
+print(r)
