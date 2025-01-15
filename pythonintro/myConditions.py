2988b669b519183fbd5c5c236a0d17849aeb00ee
ChurroBrown <26hadlejoes@washk12.org>
2025-01-15 08:45:59 -0700

    Update myConditions.py

--- pythonintro/myConditions.py
+++ pythonintro/myConditions.py
@@ -18,3 +18,26 @@ if a * b < 500:
     b = b + 10
 else:
     print("You made it to 500 or more!")
+
+#Challenge 1
+
+def agerange():
+    age = int(input("What is your age?: ")
+    if age >= 1 and age <= 12:
+              print("This is in the range of 1-12")
+    age = int(input("What is your age?: ")
+    if age >= 13 and age <= 19:
+              print("This is in the range of 13-19")
+    age = int(input("What is your age?: ")
+    if age >= 20 and age <= 35:
+              print("This is in the range of 20-35")
+    age = int(input("What is your age?: ")
+    if age >= 36 and age <= 50:
+              print("This is in the range of 36-50")
+    age = int(input("What is your age?: ")
+    if age >= 51 and age <= 65:
+              print("This is in the range of 51-65")
+    age = int(input("What is your age?: ")
+    if age >= 65:
+              print("This is in the range of 65-100+")
+
