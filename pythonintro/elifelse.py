8df0a7929852e3c37f3f4264f8b2997720eb8df8
ChurroBrown <26hadlejoes@washk12.org>
2025-01-15 15:34:31 -0700

    Update elifelse.py

--- pythonintro/elifelse.py
+++ pythonintro/elifelse.py
@@ -8,16 +8,42 @@ elif age >= 13 and age <= 25:
 elif age >= 25:
     print("You are an adult.")
 
-#Goal 2 
-def prize():
-    spin = int(input("What is your number?: "))
+#Goal 2
+
+def prize(spin):
     if spin <= 10:
-        print("Your prize is a laptop!")
+        return "Your prize is a laptop!"
     elif spin >=11 and spin <=20:
-        print("Your prize is a watch!")
+        return "Your prize is a watch!"
     elif spin >=21 and spin <=30:
-        print("Your prize is a tablet!")
+        return "Your prize is a tablet!"
     elif spin >=31 and spin <=40:
-        print("Your prize is a phone!")
+        return Your prize is a phone!"
     elif spin <=50 and spin >=41:
-        print("Your prize is a car!")
+        return "Your prize is a car!"
+
+spin = int(input("What is your number?: "))
+r = prize(spin)
+print(r)
+
+#Challenge 1
+
+def fast_food_menu(number)
+    if number == 1:
+        return "Hamburger"
+    if number == 2:
+        return "French Fries"
+    if number == 3:
+        return "Onion Rings"
+    if number == 4:
+        return "Grilled Cheese"
+    if number == 5:
+        return "Milkshake"
+    if number == 6:
+        return "Drink"
+    if number > 6:
+        return False
+
+number = int(input("What is the menu item number? ")
+food = fast_food_menu(number)
+print(food)
