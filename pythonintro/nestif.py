a78979b9e85d3933926248921a07a601257b2899
ChurroBrown <26hadlejoes@washk12.org>
2025-01-15 15:42:15 -0700

    Update nestif.py

--- pythonintro/nestif.py
+++ pythonintro/nestif.py
@@ -15,3 +15,22 @@ def carnivalride(height):
 carnivalride(27)
 carnivalride(87)
 carnivalride(12)
+
+#Challenge 1
+
+def predictor(num1, num2)
+    if num1 % 2 == 0:
+        if num2 % 2 == 0:
+            return "definitely"
+        elif num2 % 2 != 0:
+            return "likely"
+    elif num1 % 2 != 0:
+        if num2 % 2 != 0:
+            return "definitely not"
+        elif num2 % 2 == 0:
+            return "unlikely"
+
+num1 = int(input("Choose a number: ")
+num2 = int(input("Choose another number: ")
+prediction = predictor(num1, num2)
+print(prediction)
