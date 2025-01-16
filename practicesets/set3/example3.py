9a1a9ecc3413ab7d44c130fdf556ba2e4edf824f
ChurroBrown <26hadlejoes@washk12.org>
2025-01-15 17:28:35 -0700

    Update example3.py

--- practicesets/set3/example3.py
+++ practicesets/set3/example3.py
@@ -21,3 +21,15 @@ else:
 
 # Fixed If/Else
   
+if age >= 18 and income >= 30000 and credit_score >= 700:
+    print("Loan approved.")
+elif age >= 18 and income >= 30000 and credit_score >= 600:
+    print("Loan approved with higher interest.")
+elif age >= 18 and income >= 30000 and credit_score < 600:
+    print("Loan denied due to low credit score.")
+elif age >= 18 and income < 30000:
+    print("Loan denied due to insufficient income.")
+elif age < 18:
+    print("Loan denied due to age restriction.")
+else:
+    print("Loan denied.")
