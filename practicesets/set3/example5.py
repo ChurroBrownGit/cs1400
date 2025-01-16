c59df6dacb1c6d621b04ad7c76f00058eddc98d4
ChurroBrown <26hadlejoes@washk12.org>
2025-01-15 17:41:39 -0700

    Update example5.py

---
+++ practicesets/set3/example5.py
@@ -0,0 +1,38 @@
+# Inputs
+season = input("Enter the season (summer, winter, spring, fall): ").lower()
+temperature = int(input("Enter the temperature (F): "))
+humidity = int(input("Enter the humidity percentage: ")) if season == "summer" else None
+snow = input("Is there snow? (yes/no): ").lower() if season == "winter" else ""
+
+# Nested If/Else
+
+if season == "summer":
+    if temperature > 85:
+        if humidity < 60:
+            print("Suggestion: Go to the beach.")
+        else:
+            print("Suggestion: Stay indoors with AC.")
+    else:
+        print("Suggestion: Enjoy an outdoor picnic.")
+elif season == "winter":
+    if snow == "yes":
+        print("Suggestion: Go skiing.")
+    else:
+        print("Suggestion: Build a snowman.")
+else:
+    print("Suggestion: Enjoy a walk outside.")
+
+# Fixed If/Else
+
+if season == "summer" and temperature > 85 and humidity < 60:
+  print("Suggestion: Go to the beach.")
+elif season == "summer" and temperature > 85 and humidity > 60:
+  print("Suggestion: Stay indoors with AC.")
+elif season == "summer" and temperature < 85:
+  print("Suggestion: Enjoy an outdoor picnic.")
+elif season == "winter" and snow == "yes":
+  print("Suggestion: Go skiing.")
+elif season == "winter" and snow != "yes":
+  print("Suggestion: Build a snowman.")
+else:
+  print("Suggestion: Enjoy a walk outside.")
