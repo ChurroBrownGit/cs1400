df356db4380914084fa765ec826d347e79f9c3e9
ChurroBrown <26hadlejoes@washk12.org>
2025-01-22 09:14:31 -0700

    oops i didnt save

---
+++ pythonbasics2/myLoops.py
@@ -0,0 +1,21 @@
+#Goal 1
+for i in range(5):
+    print("Joel")
+
+#Goal 2
+for number in range(0,100):
+    print(number)
+
+#Challenge 1
+def spam(integer):
+    for number in range(integer):
+        print("spam")
+
+integer = int(input("What is the integer?"))
+spam(integer)
+
+def detention():
+    for i in range(0,500):
+        print("I will not waste chalk")
+
+detention()
