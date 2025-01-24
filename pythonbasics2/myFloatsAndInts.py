e3a6d8ed741e0eb793f50c7a4116e2d5034d5923
ChurroBrown <26hadlejoes@washk12.org>
2025-01-24 11:25:32 -0700

    push

--- pythonbasics2/myFloatsAndInts.py
+++ pythonbasics2/myFloatsAndInts.py
@@ -8,3 +8,33 @@ burritos = 39
 people = 7
 burritos_per_person = burritos_for_each(burritos, people)
 print(burritos_per_person)
+
+#Goal 2
+def give_me_5(num):
+    num = num + 5
+    return num
+
+num = int(input("What is the number?"))
+num = give_me_5(num)
+print(num)
+
+#Goal 3
+def chop(number):
+    number = number - int(number)
+    return number
+
+number = float(input("What float do you choose? "))
+number = chop(number)
+print(number)
+
+#Challenge 1
+def roundup(number2):
+    number2 = number2 + 1
+    number2 = number2 // 1
+    return number2
+
+number2 = float(input("What float number? "))
+number2 = roundup(number2)
+print(number2)
+
+#Challenge 2
