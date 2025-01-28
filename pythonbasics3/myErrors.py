271f6a52a4fab173c96fe17592a742e834048a74
ChurroBrown <26hadlejoes@washk12.org>
2025-01-27 20:31:15 -0700

    commit

---
+++ pythonbasics3/myErrors.py
@@ -0,0 +1,13 @@
+#Goal 1
+print(1/0)
+#This gives a ZeroDivisionError.
+
+#Goal 2
+name = "Joel"
+age = 16
+sentence = name + age
+print(sentence)
+#This gives a TypeError.
+
+#Challenge 1
+#You can use "except IndexError:" to change error messages to print statements.
