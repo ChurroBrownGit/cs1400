053339ce5203ef16344971b9448bb844d4682f53
ChurroBrown <26hadlejoes@washk12.org>
2025-01-28 22:37:58 -0700

    last of tuesday

---
+++ pythonbasics3/myCompoundOperators.py
@@ -0,0 +1,21 @@
+#Goal 1
+x = 0
+while x < 10:
+    x += 1
+    print(x)
+
+#Goal 2
+y = 200
+while y > 1:
+    y /= 2
+    print(y)
+
+#Challenge 1
+def fibonacci(x):
+    y = 0
+    z = 1
+    while y <= x:
+        y, z = z, y + z
+        print(y)
+    
+fibonacci(200)
