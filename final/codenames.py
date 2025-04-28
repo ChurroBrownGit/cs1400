57dc164c3377713df48c35808746a9a2cfa7a6f7
Churro <26hadlejoes@washk12.org>
2025-04-28 09:20:06 -0600

    far

--- final/codenames.py
+++ final/codenames.py
@@ -2,21 +2,12 @@
 #Rob Combs, Joel Hadley, Zach Sherratt
 
 #GAMEPLAN:
-#Step 1: Setup
-    #-Make randomized board - DONE
-    #-List of codenames - DONE
-    #-Random spy location generator - 
-#Step 2: Players
-    #-Create teams, num of players
-    #-Choose names
-    #-Choose hint guy and guess guy
 #Step 3: Play
     #-Who goes first?
     #-Assign 2 people to a turn
     #-Give hint
     #-Guess
     #-Correct or Incorrect?
-    #-Assassin location
 #Step 4: Winner
     #-Which team found all agents
     #-End game
@@ -44,9 +35,48 @@ def makeboard(filename):
         codewords5 = random.sample(words, 5)
         print(codewords5)
 
-makeboard("codenames.txt")
+newwordboard = makeboard("codenames.txt")
 
-def spyboard()
+def spyboard():
+    num_blue = random.choice([8, 9])
+    num_red = 17 - num_blue
+    num_assassin = 1
+    num_civilian = 25 - (num_blue + num_red + num_assassin)
+    roles = ["B"] * num_blue + ["R"] * num_red + ["C"] * num_civilian + ["A"]
+    random.shuffle(roles)
+    return roles, num_blue
 
-def turns():
+def print_board(board):
+    for i in range(0, 25, 5):
+        print(board[i:i+5])
 
+newspyboard, num_blue = spyboard()
+print_board(newspyboard)
+
+def players(num_blue):
+    numofplayers = int(input("How many players will be playing?: "))
+    names = []
+    first = "0"
+    for i in range(numofplayers):
+        name = input("What is thou name?: ")
+        names.append(name)
+    random.shuffle(names)
+    half = numofplayers // 2
+    teamred = names[:half]
+    teamblue = names[half:]
+    if num_blue == 8:
+        first = "Red"
+    elif num_blue == 9:
+        first = "Blue"
+    return teamred, teamblue, first
+
+teamblue, teamred, first = players(num_blue)
+print("Team 1:", teamred)
+print("Team 2:", teamblue)
+print("Team " + first + " will go first.")
+
+def turns(newwordboard, newspyboard, teamblue, teamred, first): 
+    codegiver = random.randint(teamblue)
+    
+    for item in teamblue:
+        guesser=random.randint(teamblue)
